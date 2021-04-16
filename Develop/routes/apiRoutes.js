32a7797ae64e3ed96072fddd28967cf28e099afc
Natalie Finnegan <nfinneg2@gmail.com>
2021-04-16 13:05:22 -0600

    working on saving a note

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -2,14 +2,29 @@ const fs = require("fs");
 const path = require("path");
 const router = require("express").Router();
 const notes = require("../db/db.json");
-// const notes = JSON.parse(
-//   fs.readFileSync(path.join(__dirname, "../db/db.json")),
-//   "utf-8"
-// );
+const uuid = require("uuid");
 
+//const allNotes = [];
+
+//Gets all notes
 router.get("/", (req, res) => {
   res.json(notes);
   console.log(notes);
+});
+
+//Create Note
+router.post("/", (req, res) => {
+  const newNote = {
+    id: uuid.v4(),
+    note: req.body.name,
+    subNote: req.body.subNote,
+  };
+
+  if (!newNote.name || !newNote.subNote) {
+    return res.status(400).json({ msg: "Please enter a note" });
+  }
+  notes.push(newNote);
+  res.json(notes);
 });
 
 module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -7,12 +7,17 @@ const htmlRoutes = require("../Develop/routes/htmlRoutes");
 
 const PORT = process.env.PORT || 3000;
 
+//Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
+//Body Parser Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//Notes API routes
 app.use("/api/notes", apiRoutes);
+
+//HTML routes
 app.use("/", htmlRoutes);
 
 app.listen(PORT, () => {
