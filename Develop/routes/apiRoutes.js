6dd2d9b8f4f96816de47b0dfd530a18d4ff31efa
Natalie Finnegan <nfinneg2@gmail.com>
2021-04-16 20:34:47 -0600

    strting on delete

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -14,24 +14,53 @@ router.get("/", (req, res) => {
 
 //Create Note
 router.post("/", (req, res) => {
-  let existingNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
+  let existingNotes;
   const newNote = req.body;
   console.log(newNote);
-  existingNotes.push(newNote);
 
-  fs.writeFileSync("./db/db.json", JSON.stringify(existingNotes), "utf-8");
-  res.json(existingNotes);
-  console.log(existingNotes);
+  fs.readFile("./db/db.json", function (err, data) {
+    if (err) {
+      throw err;
+    }
+    existingNotes = JSON.parse(data);
+
+    existingNotes.push(newNote);
+    console.log(existingNotes);
+
+    fs.writeFile("./db/db.json", JSON.stringify(existingNotes), (err) =>
+      err ? console.log(err) : console.log("Done")
+    );
+  });
+
+  res.send(existingNotes);
+
+  //   let existingNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
+  //   const newNote = req.body;
+  //   console.log(newNote);
+  //   existingNotes.push(newNote);
+
+  //   fs.writeFileSync("./db/db.json", JSON.stringify(existingNotes), "utf-8");
+  //   res.json(existingNotes);
+  //   console.log(existingNotes);
 });
 
 //Delete Note
 router.delete("/:id", (req, res) => {
-  let found = req.params.id;
   let existingNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
-  let newSavedNotes = filter((existingNotes) => existingNotes !== found);
-  existingNotes.push(newSavedNotes);
-  fs.writeFileSync("./db/db.json", JSON.stringify(existingNotes), "utf-8");
-  res.json(existingNotes);
+
+  let foundID = req.params.id;
+  console.log(foundID);
+  let updatedNotes = existingNotes.filter(
+    (existingNotes) => existingNotes !== foundID
+  );
+
+  console.log(updatedNotes);
+  existingNotes = updatedNotes;
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(updatedNotes), "utf-8");
+
+  res.json(updatedNotes);
+  console.log(updatedNotes);
 });
 
 module.exports = router;
